df41e7b937613762d1f2922bf747576f5a03facd
unknown <satthawutluangrattana@kkumail.com>
2020-09-24 15:57:50 +0700

    submit midterm

---
+++ satthawut-midterm-2020/lib/order.js
@@ -0,0 +1,8 @@
+while (true) {
+    let n = prompt("Enter a positive interger")
+
+    if (n > 0 && n % 1 == 0) {
+            alert("You would like to order " + n + " books");
+            break;
+        }
+    }
